75541af9558f348066e26c250f6ae0d82f7cb5bb
Hamskid1 <hameedlateef343@gmail.com>
2026-05-11 19:24:35 +0100

    creating transaction routes

--- controllers/client.js
+++ controllers/client.js
@@ -1,8 +1,9 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
 export const getProducts = async (req, res) => {
-     try {
+    try {
         const products = await Product.find();
         const productsWithStats = await Promise.all(
             products.map(async (product) => {
@@ -14,6 +15,14 @@ export const getProducts = async (req, res) => {
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-}
+};
 
 
+export const getCustomers = async (req, res) => {
+    try {
+        const customers = await User.find({ role: "user" }).select("-password");
+        res.status(200).json(customers);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
